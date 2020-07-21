1c06c1fa0a1593a761fe0dd2c41ce8b2987e83c6
josep-itacademy <josep.colome.feina@gmail.com>
2020-07-21 14:08:15 +0200

    deleted old files added two new

--- Stube/stube1.sql
+++ Stube/stube1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Temps de generació: 03-07-2020 a les 19:36:10
+-- Temps de generació: 21-07-2020 a les 14:04:27
 -- Versió del servidor: 10.1.36-MariaDB
 -- Versió de PHP: 7.2.10
 
@@ -25,6 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de la taula `llistat`
+--
+
+CREATE TABLE `llistat` (
+  `email` varchar(100) NOT NULL,
+  `id_video` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de la taula `Usuari`
 --
 
@@ -36,20 +47,26 @@ CREATE TABLE `Usuari` (
 -- --------------------------------------------------------
 
 --
--- Estructura de la taula `VideosUsuari`
+-- Estructura de la taula `video`
 --
 
-CREATE TABLE `VideosUsuari` (
-  `email` varchar(100) NOT NULL,
-  `titol` varchar(100) NOT NULL,
-  `descripcio` varchar(250) NOT NULL,
-  `url` varchar(50) NOT NULL,
-  `video_id` varchar(12) NOT NULL
+CREATE TABLE `video` (
+  `video_id` varchar(12) NOT NULL,
+  `titol` varchar(50) NOT NULL,
+  `descripcio` varchar(500) NOT NULL,
+  `url` varchar(44) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índexs per a les taules bolcades
 --
+
+--
+-- Índexs per a la taula `llistat`
+--
+ALTER TABLE `llistat`
+  ADD PRIMARY KEY (`email`,`id_video`),
+  ADD KEY `id_video` (`id_video`);
 
 --
 -- Índexs per a la taula `Usuari`
@@ -58,22 +75,22 @@ ALTER TABLE `Usuari`
   ADD PRIMARY KEY (`email`);
 
 --
--- Índexs per a la taula `VideosUsuari`
+-- Índexs per a la taula `video`
 --
-ALTER TABLE `VideosUsuari`
+ALTER TABLE `video`
   ADD PRIMARY KEY (`video_id`),
-  ADD UNIQUE KEY `video_id` (`video_id`),
-  ADD KEY `email` (`email`);
+  ADD UNIQUE KEY `video_id` (`video_id`);
 
 --
 -- Restriccions per a les taules bolcades
 --
 
 --
--- Restriccions per a la taula `VideosUsuari`
+-- Restriccions per a la taula `llistat`
 --
-ALTER TABLE `VideosUsuari`
-  ADD CONSTRAINT `videosusuari_ibfk_1` FOREIGN KEY (`email`) REFERENCES `Usuari` (`email`);
+ALTER TABLE `llistat`
+  ADD CONSTRAINT `llistat_ibfk_1` FOREIGN KEY (`email`) REFERENCES `Usuari` (`email`),
+  ADD CONSTRAINT `llistat_ibfk_2` FOREIGN KEY (`id_video`) REFERENCES `video` (`video_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
